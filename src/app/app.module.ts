e80371a60c10bbc545b6bbd69515dc00e3b3396a
unknown <yvonnegamboeck@web.de>
2021-03-25 21:18:34 +0100

    menu- & startscreen componentadded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartscreenComponent } from './startscreen/startscreen.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartscreenComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
