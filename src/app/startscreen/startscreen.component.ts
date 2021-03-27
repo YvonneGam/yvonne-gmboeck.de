74783c89e4d86b2d0081ffbfab82b378d19e9191
unknown <yvonnegamboeck@web.de>
2021-03-27 13:12:22 +0100

    add animation to startscreen

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./startscreen.component.scss']
 })
 export class StartscreenComponent implements OnInit {
+  greetings = ['Hi,', 'Servus,', 'Hello,'];
+  currentText = 0;
+  showText = true;
+
+
+
+  ngOnInit(): void {
+    this.changeGreetings();
+  }
+
+  /**
+   * Changes the greeting-text on init every 8seconds
+   */
+  changeGreetings() {
+
+    setInterval(() => {
+      this.currentText++;
+      this.currentText = this.currentText % this.greetings.length;
+      this.showText = false;
+
+      setTimeout(() => {
+        this.showText = true;
+      }, 1);
+
+    }, 4000);
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
