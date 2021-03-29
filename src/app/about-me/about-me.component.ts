53e191e6a45d9f2ccc6ca5999b690d82b3f84e56
unknown <yvonnegamboeck@web.de>
2021-03-29 19:02:54 +0200

    change arrangement

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -10,6 +10,23 @@ export class AboutMeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+/*     this.mouseOver(); */
   }
 
+  /**
+   * Button Hover effect "about-me"
+   */
+/*   mouseOver() {
+    console.log('test JS');
+
+    @ViewChild('btn') btn: ElementRef;
+
+    this.btn.nativeElement.getEventListener("mousemove", (e) => {
+      xPos = e.pageX - e.target.offsetLeft;
+      yPos = e.pageY - e.target.offsetTop;
+
+      e.target.style.setProperty("--x", '${xPos}px');
+      e.target.style.setProperty("--y", '${xPos}px');
+    });
+  } */
 }
