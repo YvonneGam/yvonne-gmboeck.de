5bea8bac81a50ec77f35314a8d091f57b3720d61
unknown <yvonnegamboeck@web.de>
2021-04-03 13:05:09 +0200

    add mail-php file

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -6,27 +6,10 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent implements OnInit {
-  @ViewChild('btn')
-  btn!: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.mouseOver(); 
   }
-
-  /**
-   * Button Hover effect "about-me"
-   */
-   mouseOver() {
-    console.log('test JS');
-
-    this.btn.nativeElement.getEventListener("mousemove", (e: { pageX: number; target: { offsetLeft: number; offsetTop: number; style: { setProperty: (arg0: string, arg1: string) => void; }; }; pageY: number; }) => {
-      const xPos = e.pageX - e.target.offsetLeft;
-      const yPos = e.pageY - e.target.offsetTop;
-
-      e.target.style.setProperty("--x", '${xPos}px');
-      e.target.style.setProperty("--y", '${yPos}px');
-    });
-  } 
+ 
 }
