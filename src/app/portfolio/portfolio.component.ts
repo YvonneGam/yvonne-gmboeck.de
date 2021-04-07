fe98f5c54aa40f48c32fb312b1b54cdc7fa47cd7
unknown <yvonnegamboeck@web.de>
2021-04-07 18:02:32 +0200

    small bug fixes

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+   name = "Angular " ;
+  isInViewPort = false;
+  portfolio!: HTMLElement;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+    const portfolio = document.getElementById("portfolio");
+    console.log(this.portfolio);
 
+    window.addEventListener("scroll", () => {
+      let rect = this.portfolio.getBoundingClientRect();
+      //console.log(rect, window.innerHeight);
+      if (rect.y >= 0 && rect.y <= window.innerHeight) {
+        this.isInViewPort = true;
+        // window.removeEventListener("scroll", () => {
+        //   console.log("Ziel erreicht");
+        // }, false);
+      }
+    });
+  }
 }
+
